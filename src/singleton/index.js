5ca392d4cfd831fbd0dfe79e4e852c28f6882752
TokuyamaR <qpflapper.1007@gmail.com>
2022-11-08 00:01:08 +0900

    feat: Singletonパターンが適用されていない場合のCounterクラス

--- src/singleton/index.js
+++ src/singleton/index.js
@@ -1,3 +1,25 @@
 // Singletonパターンとは
 // ・インスタンス化を一度だけ行い、グローバルアクセスできるようなクラス
 // 考え方としては、useContextやReduxでの状態管理
+
+// Singletonパターンが適用されていない場合
+
+let counter = 0;
+
+class Counter {
+  getInstance() {
+    return this;
+  }
+
+  getCounter() {
+    return counter;
+  }
+
+  increment() {
+    return ++counter;
+  }
+
+  decrement() {
+    return --counter;
+  }
+}
