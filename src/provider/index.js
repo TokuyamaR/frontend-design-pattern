b9663c076b4b5c7381ecfacbea3e09d4a246e7c1
TokuyamaR <qpflapper.1007@gmail.com>
2022-11-11 00:15:32 +0900

    feat: providerの作成例コード

--- src/provider/index.js
+++ src/provider/index.js
@@ -4,3 +4,22 @@
 // Contextオブジェクトから提供される高階コンポーネントで、使用したい範囲をラップすることで利用できるようにする。
 // Contextオブジェクトは、ReactのcreateContextメソッドにて作成可能。
 ////////////////////////////////////////////////////////
+
+const DataContext = React.createContext();
+
+const App = () => {
+  const data = {
+    id: 1,
+    name: "Sushi",
+    price: 500,
+  };
+
+  return (
+    <div>
+      <DataContext.provider value={data}>
+        <Sidebar />
+        <MainContents />
+      </DataContext.provider>
+    </div>
+  );
+};
