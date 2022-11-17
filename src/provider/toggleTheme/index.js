2c85877f0b9659ce177c93ba258d6aee800c2194
TokuyamaR <qpflapper.1007@gmail.com>
2022-11-18 00:03:35 +0900

    chore: themeを制御するcontext追加

--- src/provider/toggleTheme/index.js
+++ src/provider/toggleTheme/index.js
@@ -4,7 +4,37 @@
 ////////////////////////////////////////////////////////
 import React from "react";
 import "./style.css";
+import List from "./List";
+
+export const ThemeContext = createContext();
+
+const themes = {
+  light: {
+    background: "#fff",
+    color: "#000",
+  },
+  dark: {
+    background: "#171717",
+    color: "#fff",
+  },
+};
 
 export default function App() {
-  return <div className="app"></div>;
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  const providerValue = {
+    theme: themes[theme],
+    toggleTheme,
+  };
+  return (
+    <div className={`App theme-${theme}`}>
+      <ThemeContext.provider value={providerValue}>
+        <List />
+      </ThemeContext.provider>
+    </div>
+  );
 }
