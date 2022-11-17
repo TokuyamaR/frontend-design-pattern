a9fb534923fe4563ac9f59841bc8990e5782ea60
TokuyamaR <qpflapper.1007@gmail.com>
2022-11-17 23:49:12 +0900

    chore: toggleTheme indexファイル追加

--- src/provider/toggleTheme/index.js
+++ src/provider/toggleTheme/index.js
@@ -4,3 +4,7 @@
 ////////////////////////////////////////////////////////
 import React from "react";
 import "./style.css";
+
+export default function App() {
+  return <div className="app"></div>;
+}
