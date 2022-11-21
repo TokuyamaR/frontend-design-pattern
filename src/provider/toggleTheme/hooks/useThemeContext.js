c6d46a449bb8468171e04b240cb40cd80255dd15
TokuyamaR <qpflapper.1007@gmail.com>
2022-11-22 00:07:01 +0900

    chore: themeがない場合のエラー処理

--- src/provider/toggleTheme/hooks/useThemeContext.js
+++ src/provider/toggleTheme/hooks/useThemeContext.js
@@ -1,6 +1,9 @@
 import ThemeContext from "../";
 
-function useThemeContext() {
+export default function useThemeContext() {
   const theme = useContext(ThemeContext);
+  if (!theme) {
+    throw new Error("useThemeContextがThemeProvider内で利用されていません。");
+  }
   return theme;
 }
