b7273ec4e2df9f223ef9c960cf5c622e266e91e1
TokuyamaR <qpflapper.1007@gmail.com>
2022-11-24 23:13:38 +0900

    feat: Prototypeパターンのclassサンプルコード

--- src/Prototype/index.js
+++ src/Prototype/index.js
@@ -2,3 +2,16 @@
 // Prototypeパターンとは
 // ・複数の同じ型のオブジェクトの間でプロパティを共有する
 /////////////////////////////////////
+
+class Human {
+  constructor(name) {
+    this.name = name;
+  }
+
+  clap() {
+    return "clap clap!";
+  }
+}
+const human1 = new Human("Emi");
+
+console.log(Human.prototype);
