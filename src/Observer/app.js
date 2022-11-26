17934e10a28c19aa06aabb5ea87b3b8795baf1a4
TokuyamaR <qpflapper.1007@gmail.com>
2022-11-26 11:57:40 +0900

    feat: Appの実装

--- src/Observer/app.js
+++ src/Observer/app.js
@@ -1,1 +1,8 @@
-export default function App() {}
+export default function App() {
+  return (
+    <div className="App">
+      <Button>Click me!</Button>
+      <FormControlLabel control={<Switch />} />
+    </div>
+  );
+}
