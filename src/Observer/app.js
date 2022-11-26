776159e251a32b253ef50b4bb5adcd039c6f115a
TokuyamaR <qpflapper.1007@gmail.com>
2022-11-26 12:24:36 +0900

    feat:subscribeでイベントを登録

--- src/Observer/app.js
+++ src/Observer/app.js
@@ -1,19 +1,36 @@
 import React from "react";
+import { Button, Switch, FormControlLabel } from "@material-ui/core";
 import { ToastContainer, toast } from "react-toastify";
+import observable from "./index";
 
 function logger(data) {
   console.log(`${Date.now()}:${data}`);
 }
 
 function toastify(data) {
-  toast(data);
+  toast(data, {
+    position: toast.POSITION.BOTTOM_RIGHT,
+    closeButton: false,
+    autoClose: 2000,
+  });
 }
+
+function handleClick() {
+  observable.notify("user click button");
+}
+
+function handleToggle() {
+  observable.notify("user toggle switch");
+}
+
+observable.subscribe(logger);
+observable.subscribe(toastify);
 
 export default function App() {
   return (
     <div className="App">
-      <Button>Click me!</Button>
-      <FormControlLabel control={<Switch />} />
+      <Button onClick={handleClick}>Click me!</Button>
+      <FormControlLabel control={<Switch onChange={handleToggle} />} />
       <ToastContainer />
     </div>
   );
