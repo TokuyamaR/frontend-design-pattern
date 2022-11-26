40ab443f0e694ca3f8ac8d1fa72c9a00beaf763a
TokuyamaR <qpflapper.1007@gmail.com>
2022-11-26 11:57:25 +0900

    feat: Observableクラスの実装

--- src/Observer/index.js
+++ src/Observer/index.js
@@ -1,5 +1,23 @@
 /*
 オブザーバパターンとは
 対象の状態が変化した際にObserverに通知するデザインパターン。
-対象の状態変化に応じて処理を実行したい場合に利用する
+対象の状態変化に応じて処理や通知を実行したい場合に利用する
 */
+
+class Observable {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(func) {
+    this.observers.push(func);
+  }
+
+  unsubscribe(func) {
+    this.observers = this.observers.filter((observer) => observer !== func);
+  }
+
+  notify(data) {
+    this.observers.forEach((observer) => observer(data));
+  }
+}
