0b98bbe863ae6c87f301a834c1ca52c51bc72607
TokuyamaR <qpflapper.1007@gmail.com>
2022-11-26 12:01:42 +0900

    chore: ログとトースト表示用関数の追加

--- src/Observer/app.js
+++ src/Observer/app.js
@@ -1,8 +1,20 @@
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+
+function logger(data) {
+  console.log(`${Date.now()}:${data}`);
+}
+
+function toastify(data) {
+  toast(data);
+}
+
 export default function App() {
   return (
     <div className="App">
       <Button>Click me!</Button>
       <FormControlLabel control={<Switch />} />
+      <ToastContainer />
     </div>
   );
 }
