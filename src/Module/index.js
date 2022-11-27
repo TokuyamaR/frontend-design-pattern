6b22d8797a32416d312bfbea8bb98e1e960aee39
TokuyamaR <qpflapper.1007@gmail.com>
2022-11-27 11:08:59 +0900

    chore: 表示用html, cssファイルの追加

--- src/Module/index.js
+++ src/Module/index.js
@@ -2,3 +2,20 @@
 モジュールパターンとは
 コードを再利用可能な小さな部品に分割する
 */
+
+// 基本的なdefault export とnamed exportなのでコードの記載は割愛
+
+// ダイナミックインポート
+// オンデマンドにモジュールをインポートできる
+
+const button = document.getElementById("btn");
+
+button.addEventListener("click", () => {
+  import("./math.js").then((module) => {
+    console.log("Add:", module.add(3, 3));
+    console.log("Multiple:", module.multiply(3));
+
+    const button = document.getElementById("btn");
+    button.innerHTML = "Check the console";
+  });
+});
