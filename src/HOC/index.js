6e9445a2a1b35afadba28c52db5f928f7421623a
TokuyamaR <qpflapper.1007@gmail.com>
2022-11-28 23:54:21 +0900

    chore: Appコンポーネントファイルの追加

--- src/HOC/index.js
+++ src/HOC/index.js
@@ -3,4 +3,24 @@ High Order Componentとは
 アプリケーションで共通の処理をpropsを通してコンポーネントに渡す
 */
 
+import React from "react";
+import { render } from "react-dom";
 
+import DogImages from "./DogImages";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Browse Dog Images{" "}
+        <span role="img" aria-label="emoji">
+          🐕
+        </span>
+      </h1>
+      <DogImages />
+    </div>
+  );
+}
+
+render(<App />, document.getElementById("root"));
