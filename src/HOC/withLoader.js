2a890aaaeabd705ce3cc1d077693d73828e5b753
TokuyamaR <qpflapper.1007@gmail.com>
2022-11-29 23:54:54 +0900

    feat: ローディング制御用HOCの追加

--- src/HOC/withLoader.js
+++ src/HOC/withLoader.js
@@ -1,4 +1,22 @@
 import React, { useEffect, useState } from "react";
 export default function withLoader(Element, url) {
-  return (props) => {};
+  return (props) => {
+    const [data, setData] = useState(null, props);
+
+    useEffect(() => {
+      async function getData() {
+        const res = await fetch(url);
+        const data = res.json();
+        setData(data);
+      }
+
+      getData();
+    }, []);
+
+    if (!data) {
+      return <div>Loading...</div>;
+    }
+
+    return <Element {...props} data={data} />;
+  };
 }
