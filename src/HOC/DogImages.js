eda5768f887f8d34bd67659bc1df7d8902626a52
TokuyamaR <qpflapper.1007@gmail.com>
2022-12-01 00:04:50 +0900

    chore: withHoverをDogImagesに適用

--- src/HOC/DogImages.js
+++ src/HOC/DogImages.js
@@ -1,13 +1,20 @@
 import React from "react";
 import withLoader from "./withLoader";
+import withHover from "./withHover";
 
 function DogImages(props) {
-  return props.data.message.map((dog, i) => (
-    <img src={dog} key={i} alt="Dog" />
-  ));
+  return (
+    <div {...props}>
+      {props.isHover && <div id="hover">Hovering!</div>}
+      <div id="list">
+        {props.data.message.map((dog, i) => (
+          <img src={dog} key={i} alt="Dog" />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default withLoader(
-  DogImages,
-  "https://dog.ceo/api/breed/labrador/images/random/6"
+export default withHover(
+  withLoader(DogImages, "https://dog.ceo/api/breed/labrador/images/random/6")
 );
