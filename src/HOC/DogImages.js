1f0777e77efa6ff49516d75c43eb24c3dabe7097
TokuyamaR <qpflapper.1007@gmail.com>
2022-12-02 00:11:48 +0900

    refactor: DogImagesをuseHoverで書き換え

--- src/HOC/DogImages.js
+++ src/HOC/DogImages.js
@@ -1,11 +1,13 @@
 import React from "react";
 import withLoader from "./withLoader";
 import withHover from "./withHover";
+import useHover from "./hooks/useHover";
 
 function DogImages(props) {
+  const [hoverRef, isHover] = useHover();
   return (
-    <div {...props}>
-      {props.isHover && <div id="hover">Hovering!</div>}
+    <div ref={hoverRef} {...props}>
+      {isHover && <div id="hover">Hovering!</div>}
       <div id="list">
         {props.data.message.map((dog, i) => (
           <img src={dog} key={i} alt="Dog" />
