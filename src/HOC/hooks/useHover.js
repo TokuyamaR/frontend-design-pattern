6273f4ad110179eb8cc57ce94201ab15ca1dc171
TokuyamaR <qpflapper.1007@gmail.com>
2022-12-02 00:08:31 +0900

    feat: useHover フックを追加

--- src/HOC/hooks/useHover.js
+++ src/HOC/hooks/useHover.js
@@ -1,2 +1,24 @@
-import React from "react";
-export default function useLoader() {}
+import React, { useState, useRef, useEffect } from "react";
+export default function useHover() {
+  const [isHover, setIsHover] = useState(false);
+  const ref = useRef(null);
+
+  const handleMouseOver = () => setIsHover(true);
+  const handleMouseOut = () => setIsHover(false);
+
+  useEffect(() => {
+    const node = ref.current();
+
+    if (node) {
+      node.addEventListener("mouseover", handleMouseOver);
+      node.addEventListener("mouseout", handleMouseOut);
+
+      return () => {
+        node.removeEventListener("mouseover", handleMouseOver);
+        node.removeEventListener("mouseout", handleMouseOut);
+      };
+    }
+  }, [ref.current]);
+
+  return [ref, isHover];
+}
