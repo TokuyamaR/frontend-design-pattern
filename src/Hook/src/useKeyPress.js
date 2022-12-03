3644aeacb880109fc584a844685b628972b990de
TokuyamaR <qpflapper.1007@gmail.com>
2022-12-03 13:23:32 +0900

    feat: keypress用のカスタムフックを追加

---
+++ src/Hook/src/useKeyPress.js
@@ -0,0 +1,26 @@
+export default function useKeyPress(targetKey) {
+  const [isKeyPressed, setIsKeyPressed] = useState(false);
+
+  function handleUp(key) {
+    if (key === targetKey) {
+      setIsKeyPressed(true);
+    }
+  }
+
+  function handleDown(key) {
+    if (key === targetKey) {
+      setIsKeyPressed(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleDown);
+    window.addEventListener("keyup", handleUp);
+    return () => {
+      window.removeEventListener("keydown", handleDown);
+      window.removeEventListener("keyup", handleUp);
+    };
+  }, []);
+
+  return [isKeyPressed];
+}
