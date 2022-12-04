86e209990bab4676f31d159902207f6a26516667
TokuyamaR <qpflapper.1007@gmail.com>
2022-12-04 12:58:35 +0900

    feat: Flyweightパターンのサンプルコードを追加

--- src/Flyweight/index.js
+++ src/Flyweight/index.js
@@ -2,3 +2,47 @@
 フライウェイとパターンとは
 同じオブジェクトを扱う際に既存のインスタンスを利用する
 */
+
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+const isbnNumbers = new Set();
+const bookList = [];
+
+const createBook = (title, author, isbn) => {
+  const hasIsbn = isbnNumbers.has(isbn);
+
+  if (hasIsbn) {
+    return hasIsbn;
+  } else {
+    const book = new Book(title, author, isbn);
+    isbnNumbers.add(isbn);
+    return book;
+  }
+};
+
+const addBook = (title, author, isbn, availability, sales) => {
+  const book = {
+    ...createBook(title, author, isbn),
+    sales,
+    availability,
+    isbn,
+  };
+
+  bookList.push(book);
+  return book;
+};
+
+addBook("Harry Potter", "JK Rowling", "AB123", false, 100);
+addBook("Harry Potter", "JK Rowling", "AB123", true, 50);
+addBook("To Kill a Mockingbird", "Harper Lee", "CD345", true, 10);
+addBook("To Kill a Mockingbird", "Harper Lee", "CD345", false, 20);
+addBook("The Great Gatsby", "F. Scott Fitzgerald", "EF567", false, 20);
+
+console.log("Total amount of copies: ", bookList.length);
+console.log("Total amount of books: ", isbnNumbers.size);
